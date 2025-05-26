b5f4baaf959c4866b10954f8dc66b8c2e8543ce0
Piyush Panchal <piyushpanchal114@gmail.com>
2025-05-26 23:13:19 +0530

    ai tools using langgraph

--- main.py
+++ main.py
@@ -1,6 +1,104 @@
-def main():
-    print("Hello from ai-tools!")
+import json
+from typing import TypedDict, Annotated
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
+from langchain.chat_models import init_chat_model
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import ToolMessage
+
+
+load_dotenv()
+
+
+tool = TavilySearch(max_results=2)
+tools = [tool]
+res = tool.invoke("What is a node in langgraph")
+
+
+llm = init_chat_model("google_genai:gemini-2.0-flash")
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+llm_with_tool = llm.bind_tools(tools)
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AI Message."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"])
+            outputs.append(ToolMessage(
+                content=json.dumps(tool_result),
+                name=tool_call["name"],
+                tool_call_id=tool_call["id"]))
+        return {"messages": outputs}
+
+
+tool_node = BasicToolNode(tools=[tool])
+
+
+def route_tools(state: State):
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tool.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot", route_tools, {"tools": "tools", END: END})
+
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+
+            stream_graph_updates(user_input)
+        except Exception:
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break

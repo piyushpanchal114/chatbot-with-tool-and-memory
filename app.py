c61332906772d0ef569d196c30172168e15d3e5c
Piyush Panchal <piyushpanchal114@gmail.com>
2025-05-27 18:56:11 +0530

    chatbot with tool and prebuilt tool node

---
+++ app.py
@@ -0,0 +1,63 @@
+from typing import TypedDict, Annotated
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
+from langchain.chat_models import init_chat_model
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+load_dotenv()
+
+tool = TavilySearch(max_results=2)
+tools = [tool]
+
+llm = init_chat_model("google_genai:gemini-2.0-flash")
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+llm_with_tool = llm.bind_tools(tools)
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tool.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+tool_node = ToolNode(tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream(
+            {"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+
+            stream_graph_updates(user_input)
+        except Exception:
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break
